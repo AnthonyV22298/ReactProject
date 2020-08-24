5c89cb961b33874ad2faf3261b5958ab942c332a
wilson.tate.dev <wilson.tate.dev@gmail.com>
2020-08-24 15:49:39 -0600

    added update contact reducers

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -1,19 +1,40 @@
-import {LOGIN_FAILED, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT} from '../constants/actionTypes';
+import {LOGIN_FAILED, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT, UPDATE_CONTACT_SUCCESS, UPDATE_CONTACT_REQUEST, UPDATE_CONTACT_FAILED} from '../constants/actionTypes';
 
+//gets the info from the local storage token
 let userInfo = JSON.parse(localStorage.getItem('token'));
-
+//sets the inital user state to token information if it exists
 const initialState = userInfo ? { loggedIn: true, userInfo } : {};
 
 export default function loginReducer(state = initialState, action){
   switch (action.type){
       case LOGIN_REQUEST:
-          return {...state, loggingIn: true};
+          return {...state,
+              loggingIn: true};
       case LOGIN_SUCCESS:
-          return {...state, loggedIn: true, userInfo: {...action.userInfo}};
+          return {...state,
+              loggedIn: true,
+              loggingIn: false,
+              loginFailed:false,
+              userInfo: {...action.userInfo}};
       case LOGIN_FAILED:
-          return {...state, loggedIn:false};
+          return {...state,
+              loginFailed:true,
+              loggingIn: false};
       case LOGOUT:
           return {};
+      case UPDATE_CONTACT_REQUEST:
+          return {...state,
+              updating: true};
+      case UPDATE_CONTACT_SUCCESS:
+          return {...state,
+              updateSuccess: true,
+              updating: false,
+              userInfo: {...action.userInfo}};
+      case UPDATE_CONTACT_FAILED:
+          return {...state,
+            updateContactFailed: true,
+            updating: false,
+            userInfo: {...action.userInfo} }
       default:
           return state
   }

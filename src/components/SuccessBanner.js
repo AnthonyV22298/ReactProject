7a4ef438dc022e3cfe62825e90667d5ee4315436
wilson.tate.dev <wilson.tate.dev@gmail.com>
2020-08-24 15:52:58 -0600

    basic success banner

---
+++ src/components/SuccessBanner.js
@@ -0,0 +1,29 @@
+"use strict"
+
+import React, { useState } from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+
+
+const SuccessBanner = (props) => {
+
+    const [hidden, setHidden] = useState(true);
+
+    return (
+        hidden ?
+            <div className="alert alert-success" role="alert">
+                <Button close onClick={() => setHidden(false)} />
+                    {props.children}
+            </div >
+            : ''
+    );
+}
+
+SuccessBanner.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+}
+
+export default SuccessBanner;

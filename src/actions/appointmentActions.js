ea07f4ff7dda30650b5ce80854e8bf880eebb130
draggling <45193915+draggling@users.noreply.github.com>
2020-08-24 19:35:28 -0400

    Converted from Contact to Appointment entity

--- src/actions/appointmentActions.js
+++ src/actions/appointmentActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { adalApiFetch } from '../adalConfig.js';
 
-import { APPOINTMENTS_SUCCESFUL, APPOINTMENTS_FAILURE, APPOINTMENTS_PENDING, POST_APPOINTMENTS_SUCCESFUL, POST_APPOINTMENTS_FAILURE} from '../constants/actionTypes';
+import { APPOINTMENTS_SUCCESFUL, APPOINTMENTS_FAILURE, APPOINTMENTS_PENDING} from '../constants/actionTypes';
 
 export const readAppointments = () => {
 
@@ -15,11 +15,14 @@ export const readAppointments = () => {
             'Prefer': "odata.include-annotations=*"
         }
     };
-    
+
     return dispatch => {
         dispatch(appointmentsPending());
         // using contact GUID 03879a5c-3aaf-ea11-a812-000d3a8e4ace (Contact "A Test")
-        return adalApiFetch(axios, "https://sstack4.crm.dynamics.com/api/data/v9.1/contacts?$select=firstname,lastname,emailaddress1,contactid,dmv_state&$filter=contains(firstname,'A')", config)
+        return adalApiFetch(axios, 
+        "https://sstack4.crm.dynamics.com/api/data/v9.1/dmv_appointments" +
+        "?$select=dmv_appointmentid,dmv_appointment_date,_dmv_contactappointmentid_value" + 
+        "&$filter=_dmv_contactappointmentid_value eq 03879a5c-3aaf-ea11-a812-000d3a8e4ace", config)
             .then(res => {
                 dispatch(appointmentsSuccess(res));
             })
@@ -30,50 +33,7 @@ export const readAppointments = () => {
     };
 }
 
-export const postAppointments = (data) => {
 
-    let config = {
-        method: 'post',
-        'OData-MaxVersion': 4.0,
-        'OData-Version': 4.0,
-        Accept: 'dmv_appointment/json',
-        'Content-Type': 'dmv_appointment/json; charset=utf-8',
-        headers: {
-            'Prefer' : 'return=representation'
-        },
-        data: {
-            "dmv_appointment_date": data.date
-        }
-    };
-    
-    return dispatch => {
-        // using contact GUID 03879a5c-3aaf-ea11-a812-000d3a8e4ace (Contact "A Test")
-        return adalApiFetch(axios, "https://sstack4.crm.dynamics.com/api/data/v9.1/dmv_appointments", config)
-            .then(res => {
-                dispatch(postAppointmentsSuccess(res));
-            })
-            .catch((error) => {
-                console.log(error);
-                dispatch(postAppointmentsFailure(error));
-            });
-        
-    };
-}
-
-
-const postAppointmentsSuccess = (res) => {
-    return {
-        type: POST_APPOINTMENTS_SUCCESFUL,
-        data:  res.data
-    };
-}
-
-const postAppointmentsFailure = (error) => {
-    return {
-        type: POST_APPOINTMENTS_FAILURE,
-        error  
-    };
-}
 
 
 const appointmentsSuccess = (res) => {

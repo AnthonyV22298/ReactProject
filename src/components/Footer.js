f9f36e74ac242b94d3b7e2a2e4345a2ff53ea95c
Chris Quintanilla <caq.dev@outlook.com>
2020-09-24 18:55:53 -0400

    Added footer and additional changes to Accordion components CSS

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+"use strict"
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3">
+            <Container>
+                <span className="text-muted">Website created by Smoothstack</span>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
